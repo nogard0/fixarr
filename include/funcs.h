d21823f8f25b5df20a516e1b0f4860bd148e01fe
Dragomir Dragnev <nogard0@gmail.com>
2023-03-16 13:36:20 +0200

    add ULFIUS_CHECK_VERSION if missing

--- include/funcs.h
+++ include/funcs.h
@@ -12,4 +12,12 @@ void print_response(struct _u_response * response);
 time_t convert_iso8601(const char *time_string);
 char *secs_to_hrtime(time_t sec);
 
+#ifndef ULFIUS_CHECK_VERSION
+#define ULFIUS_CHECK_VERSION(major,minor,patch)                        \
+  (ULFIUS_VERSION_MAJOR > (major) ||                                    \
+   (ULFIUS_VERSION_MAJOR == (major) && ULFIUS_VERSION_MINOR > (minor)) || \
+   (ULFIUS_VERSION_MAJOR == (major) && ULFIUS_VERSION_MINOR == (minor) && \
+    ULFIUS_VERSION_PATCH >= (patch)))
+#endif
+
 #endif /* FUNCS_H */
